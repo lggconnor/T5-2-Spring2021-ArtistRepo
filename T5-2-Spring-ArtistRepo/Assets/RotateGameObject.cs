09e6daa58ddf34f151dd2d5c42ffeb0879d102ae
lggconnor <32574244+lggconnor@users.noreply.github.com>
2021-04-11 12:46:03 -0400

    niceee

--- T5-2-Spring-ArtistRepo/Assets/RotateGameObject.cs
+++ T5-2-Spring-ArtistRepo/Assets/RotateGameObject.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class RotateGameObject : MonoBehaviour
 {
-
     public float rotateSpeed;
     public Vector3 rotate;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         this.transform.Rotate(rotate * rotateSpeed);
